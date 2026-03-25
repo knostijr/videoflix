792fe81106cb8edbefc8e091613695dbcd4b346e
knostijr <christophkonst@icloud.com>
2026-03-25 18:24:55 +0100

    register video signals in app ready hook

--- videos/apps.py
+++ videos/apps.py
@@ -1,5 +1,14 @@
+"""App configuration for the videos app."""
+
 from django.apps import AppConfig
 
 
 class VideosConfig(AppConfig):
+    """Configuration class for the videos Django application."""
+
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'videos'
+
+    def ready(self):
+        """Import signal handlers when Django has finished loading."""
+        import videos.signals  # noqa: F401
